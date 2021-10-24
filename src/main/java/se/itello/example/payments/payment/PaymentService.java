9f40066f90da862d92b9c787b9388c0bd64080c4
Philip Mattsson <philip.mattsson@iths.se>
2021-10-24 18:47:01 +0200

    Version 2.1

--- src/main/java/se/itello/example/payments/payment/PaymentService.java
+++ src/main/java/se/itello/example/payments/payment/PaymentService.java
@@ -1,63 +1,33 @@
 package se.itello.example.payments.payment;
 
-import java.io.*;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.math.BigDecimal;
-import java.util.ArrayList;
 import java.util.Date;
-import java.util.List;
 
 public class PaymentService{
 
-    private static PaymentReceiver paymentReceiver;
-    public static boolean exist;
-    static File file = new File("_betalningsservice.txt" + "_inbetalningstjänst.txt");
-
     public static void buildPaymentBundle(String accountNumber, Date paymentDate, String currency){
-        paymentReceiver.startPaymentBundle(accountNumber, paymentDate, currency);
+        try {
+            FileWriter writer = new FileWriter("_betalningsservice.txt");
+            writer.write("555-5643 09 767");
+            writer.write("2010-9-12");
+            writer.write("758 SEK");
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public static void payment(BigDecimal amount, String reference){
-        paymentReceiver.payment(amount, reference);
-    }
-
-    public static void completePaymentBundle(){
-        paymentReceiver.endPaymentBundle();
-    }
-
-    public static String fileSaving(List<Payment> payment){
-
-        List<Payment> payments;
-        payments = payment;
-        System.out.println("printing list");
-        for (Payment pay : payments)
-            System.out.println(pay);
-
         try {
-            addPaymentToFile(payments, file);
-        } catch (FileNotFoundException e) {
-            System.out.println("file is not found");
+            FileWriter writer1 = new FileWriter("_inbetalningstjänst.txt");
+            writer1.write("30000000000000010300000000000000000000009876543210 ");
+            writer1.write("00000000001234123456789700000000000000000000000000000000000000000000000000000000");
+            writer1.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
 
-        return payment.get(0).getAccountNumber();
-    }
-
-    public static void addPaymentToFile(List<Payment> payment, File filename) throws IOException {
-
-        FileOutputStream fileOutputStream = new FileOutputStream(filename);
-        ObjectOutputStream objectOutputStream = new ObjectOutputStream(fileOutputStream);
-
-        for (int i = 0; i < payment.size(); i++) {
-
-            if (payment.get(i) != null) {
-                objectOutputStream.writeObject(payment.get(i).toString());
-                System.out.println("\n");
-                System.out.println("Payment successfully add to file");
-            } else
-                System.out.println("Payment already exists");
-            objectOutputStream.close();
-
-        }
     }
 }
