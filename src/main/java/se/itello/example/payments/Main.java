cf2db6ca2e03f138708ceaa2c39ddd1a0ed939ad
Philip Mattsson <philip.mattsson@iths.se>
2021-10-24 09:13:25 +0200

    Updated Main

--- src/main/java/se/itello/example/payments/Main.java
+++ src/main/java/se/itello/example/payments/Main.java
@@ -1,21 +1,29 @@
 package se.itello.example.payments;
 
+import se.itello.example.payments.payment.Payment;
 import se.itello.example.payments.payment.PaymentService;
 
+import java.io.IOException;
 import java.math.BigDecimal;
 import java.util.Date;
+import java.util.List;
 
 
 public class Main {
 
-    public static void main(String[] args){
+    public static void main(String[] args) throws IOException {
 
-        PaymentService.buildPaymentBundle("555-5643 09 767",
-                new Date("2010-09-12"), "758 SEK");
+        try {
+            PaymentService.buildPaymentBundle("555-5643 09 767",
+                    new Date("2010-09-12"), "758 SEK");
 
-        PaymentService.payment(new BigDecimal("30000000000000010300000000000000000000009876543210"),
-                "00000000001234123456789700000000000000000000000000000000000000000000000000000000");
+            PaymentService.payment(new BigDecimal("30000000000000010300000000000000000000009876543210"),
+                    "00000000001234123456789700000000000000000000000000000000000000000000000000000000");
 
-        PaymentService.completePaymentBundle();
+            PaymentService.completePaymentBundle();
+        } catch (){
+
+        }
+
     }
 }
--- src/main/java/se/itello/example/payments/payment/PaymentReceiver.java
+++ src/main/java/se/itello/example/payments/payment/PaymentReceiver.java
@@ -10,18 +10,18 @@ public interface PaymentReceiver {
      * @param paymentDate The date at which the funds were/will be deposited on the specified account.
      * @param currency The currency of the payments in the bundle.
      */
-    public void startPaymentBundle(String accountNumber, Date paymentDate, String currency);
+    void startPaymentBundle(String accountNumber, Date paymentDate, String currency);
 
     /**
      * Called to notify the receiver of a single payment within a bundle.
      * @param amount The payment amount.
      * @param reference The payment reference.
      */
-    public void payment(BigDecimal amount, String reference);
+    void payment(BigDecimal amount, String reference);
 
     /**
      * Called to mark the end of a payment bundle. This means that there will be no more calls to payment() 
      * for this bundle, and that the receiver can process the bundle.
      */
-    public void endPaymentBundle();
+    void endPaymentBundle();
 }
